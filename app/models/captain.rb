07a47a43b8288c979e3e42f82bdae3ab48b3fe74
Joey Cho <cjy12182000@naver.com>
2019-02-25 09:28:26 +0000

    Automatically backed up by Learn

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,20 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    joins(boats: :classifications).where(classifications: {name: 'Catamaran'})
+  end
+
+  def self.sailors
+    joins(boats: :classifications).where(classifications: {name: 'Sailboat'}).uniq
+      end
+
+  def self.talented_seafarers
+    binding.pry
+    joins(boats: :classifications).where(classifications: {name: include?(['Motorboat','Sailboat'])}).where(classifications: {name: 'Sailboat'}).uniq
+
+    sailors.joins(boats: :classifications).where(classifications: {name: 'Motorboat'} ).uniq
+  end
+
+
 end
