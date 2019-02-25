d39ca87cef03c2eb774cb462f6cfce70e348aa24
Joey Cho <cjy12182000@naver.com>
2019-02-25 10:34:00 +0000

    Automatically backed up by Learn

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -10,7 +10,7 @@ class Captain < ActiveRecord::Base
       end
 
   def self.talented_seafarers
-    joins(boats: :classifications).group('classifications.name').where("classifications.name = ? OR classifications.name = ?",'Motorboat','Sailboat').order('captain.name DESC')
+    Captain.joins(boats: :classifications).group('classifications.name').order('name ASC').where("classifications.name = ? OR classifications.name = ?",'Motorboat','Sailboat')
   end
 
 
