21dbf215d740b75962e2e34968cf088e24278609
Joey Cho <cjy12182000@naver.com>
2019-02-25 13:03:37 +0000

    Automatically backed up by Learn

--- app/models/classification.rb
+++ app/models/classification.rb
@@ -2,10 +2,11 @@ class Classification < ActiveRecord::Base
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
 
-  eturns all classifications
-  returns the classifications for the longest boat
-  ::my_all
   def self.my_all
+    self
+  end
+
+  def self.longest
     self
   end
   
